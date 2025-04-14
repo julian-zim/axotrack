92e3126731bd43ba2542fa1fded2f665ce517422
Julian Z <julian.zimmermann.tue@gmail.com>
2025-04-14 20:54:23 +0200

    Added Shortcut creation & manual to install script

--- install/build.ps1
+++ install/build.ps1
@@ -1,3 +1,13 @@
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process -Force
 conda activate axotrack
 pyinstaller --name="AxoTrack" --icon="icon.ico" "../main.py" --onedir --noconsole
+Copy-Item -Path "..\manual\manual.pdf" -Destination ".\dist\AxoTrack" -ErrorAction SilentlyContinue
+Start-Process "explorer.exe" -ArgumentList ".\dist\AxoTrack"
+
+$desktopPath = [Environment]::GetFolderPath("Desktop")
+Copy-Item -Path ".\dist\AxoTrack" -Destination "$desktopPath\AxoTrack\" -Recurse
+
+$wshShell = New-Object -ComObject WScript.Shell
+$shortcut = $wshShell.CreateShortcut("$desktopPath\AxoTrack.lnk")
+$shortcut.TargetPath = "$desktopPath\AxoTrack\AxoTrack.exe"
+$shortcut.Save()
